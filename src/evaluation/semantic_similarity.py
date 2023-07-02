1f7a1d54333a0713f7417667eaa55891b1284579
Leon Knorr <43034754+lkno0705@users.noreply.github.com>
2023-07-02 13:53:22 +0200

    Remove reference sentence transformer code

--- src/evaluation/semantic_similarity.py
+++ src/evaluation/semantic_similarity.py
@@ -5,28 +5,6 @@ from sentence_transformers import SentenceTransformer, util
 from src.evaluation.evaluator import Evaluator
 import torch
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-# Two lists of sentences
-sentences1 = ['The cat sits outside',
-              'A man is playing guitar',
-              'The new movie is awesome']
-
-sentences2 = ['The dog plays in the garden',
-              'A woman watches TV',
-              'The new movie is so great']
-
-# Compute embedding for both lists
-embeddings1 = model.encode(sentences1, convert_to_tensor=True)
-embeddings2 = model.encode(sentences2, convert_to_tensor=True)
-
-# Compute cosine-similarities
-cosine_scores = util.cos_sim(embeddings1, embeddings2)
-
-# Output the pairs with their score
-for i in range(len(sentences1)):
-    print("{} \t\t {} \t\t Score: {:.4f}".format(sentences1[i], sentences2[i], cosine_scores[i][i]))
-
 
 class SentenceTransformerEvaluator(Evaluator):
     """
@@ -43,7 +21,6 @@ class SentenceTransformerEvaluator(Evaluator):
 
     def evaluate(self, model_output: List[Tuple[int, List[str]]], references: List[Tuple[int, List[str]]]) -> Dict[
         str, float]:
-
         def extract_strings(inp_collection: List[Tuple[int, List[str]]]):
             """
                 Select the first string from each tuple in the input collection, as evaluation example.
@@ -65,7 +42,7 @@ class SentenceTransformerEvaluator(Evaluator):
             "avg_cos_sim": torch.diag(self.similiarities).mean(),
             "max_cos_sim": torch.diag(self.similiarities).max(),
             "min_cos_sim": torch.diag(self.similiarities).min()
-            }
+        }
 
     def save_similarities_to_file(self, path='out/eval/cosine_sim.csv'):
         """
@@ -81,10 +58,3 @@ class SentenceTransformerEvaluator(Evaluator):
             "reference": self.sentences_from_reference,
             "similiarities": similiarities_diag
         }).to_csv(path_or_buf=path, index=False)
-
-
-
-
-
-
-
