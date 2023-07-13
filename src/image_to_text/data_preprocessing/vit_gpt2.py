d924cfc752c1bdc9fb14c5b6024e4a2296f7e583
Leon Knorr <43034754+lkno0705@users.noreply.github.com>
2023-07-13 11:08:34 +0200

    fix vit-gpt2 dataset base class

--- src/image_to_text/data_preprocessing/vit_gpt2.py
+++ src/image_to_text/data_preprocessing/vit_gpt2.py
@@ -2,12 +2,12 @@ from abc import ABC, abstractmethod
 from typing import List, Tuple
 
 from pypdfium2 import PdfDocument
-from util import extract_text
+from src.image_to_text.data_preprocessing.util import extract_text
 from tqdm import tqdm
 import pandas as pd
 from bs4 import BeautifulSoup
 from datasets import load_dataset
-from PIL import Image
+from PIL.Image import Image
 import torch
 from sklearn.model_selection import train_test_split
 from os import makedirs, path
@@ -15,11 +15,14 @@ from os import makedirs, path
 
 class VitGPT2Dataset(ABC):
 
-    def __init__(self, slide_path: str, dataset_path='/datasets/gold_standard/', max_target_length=128):
+    def __init__(self, slide_path: str, dataset_path='/datasets/', max_target_length=128):
         self.extracted_test = None
         self.extracted_val = None
         self.extracted_train = None
         self.dataset = None
+        self.train_metadata = None
+        self.test_metadata = None
+        self.val_metadata = None
         self.max_target_length = max_target_length
         self.dataset_path = dataset_path
         self.pdf = PdfDocument(slide_path)
@@ -28,7 +31,7 @@ class VitGPT2Dataset(ABC):
         self.val_path = path.join(dataset_path, "val")
 
     @abstractmethod
-    def build_metadata_csv(self, split_path: str, split_page_numbers: List[int]):
+    def build_metadata_csv(self, split_path: str, split_page_numbers: List[int]) -> pd.DataFrame:
         """
         This class builds the metadata.csv which defines the non-image features of the dataset.
         In this case, the file should contain two columns:
@@ -44,14 +47,14 @@ class VitGPT2Dataset(ABC):
             slide_image.save(path.join(dir_path, f"slide_{slide_nr}.png"))
 
     @staticmethod
-    def _extract_slide_page_numbers(split_extracted_content:  list[tuple[int, str, str, Image]]) -> List[int]:
-        return [page_nr for page_nr,_,_,_ in split_extracted_content]
+    def _extract_slide_page_numbers(split_extracted_content: List[tuple[int, str, str, Image]]) -> List[int]:
+        return [page_nr for page_nr, _, _, _ in split_extracted_content]
 
     @staticmethod
-    def preprocess(feature_extractor, tokenizer, samples, max_target_length):
+    def preprocess(samples, feature_extractor, tokenizer, max_target_length):
         """ Preprocess the dataset to be used with vit-gpt2 """
         images = samples["image"]
-        questions = samples["Questions"]
+        questions = samples["Question"]
 
         return {
             "labels": tokenizer(questions, padding="max_length", max_length=max_target_length).input_ids,
@@ -59,7 +62,6 @@ class VitGPT2Dataset(ABC):
         }
 
     def build_dataset(self, feature_extractor, tokenizer):
-
         extracted = extract_text(self.pdf.raw)
 
         self.extracted_train, val_test = train_test_split(extracted, test_size=0.4, train_size=0.6)
@@ -69,29 +71,34 @@ class VitGPT2Dataset(ABC):
         makedirs(self.val_path, exist_ok=True)
         makedirs(self.test_path, exist_ok=True)
 
-        self._save_files_to_dir(dir_path=self.train_path, extracted_content=self.extracted_train)
-        self._save_files_to_dir(dir_path=self.test_path, extracted_content=self.extracted_test)
-        self._save_files_to_dir(dir_path=self.val_path, extracted_content=self.extracted_val)
-
-        self.build_metadata_csv(
+        self.train_metadata = self.build_metadata_csv(
             split_path=self.train_path,
             split_page_numbers=self._extract_slide_page_numbers(self.extracted_train)
         )
-        self.build_metadata_csv(
+        self.test_metadata = self.build_metadata_csv(
             split_path=self.test_path,
             split_page_numbers=self._extract_slide_page_numbers(self.extracted_test)
         )
-        self.build_metadata_csv(
+        self.val_metadata = self.build_metadata_csv(
             split_path=self.val_path,
             split_page_numbers=self._extract_slide_page_numbers(self.extracted_val)
         )
 
-        dataset = load_dataset("image_folder", data_dir=self.dataset_path)
-        dataset = dataset.filter(lambda sample: sample["Questions"] is not None)  # Filter None Values
+        # filter unnecessary slides
+        self.extracted_train = [t for t in self.extracted_train if t[0] in self.train_metadata["Page Number"].to_list()]
+        self.extracted_test = [t for t in self.extracted_test if t[0] in self.test_metadata["Page Number"].to_list()]
+        self.extracted_val = [t for t in self.extracted_val if t[0] in self.val_metadata["Page Number"].to_list()]
+
+        self._save_files_to_dir(dir_path=self.train_path, extracted_content=self.extracted_train)
+        self._save_files_to_dir(dir_path=self.test_path, extracted_content=self.extracted_test)
+        self._save_files_to_dir(dir_path=self.val_path, extracted_content=self.extracted_val)
+
+        dataset = load_dataset("imagefolder", data_dir=self.dataset_path)
         self.dataset = dataset.map(
             function=self.preprocess,
             batched=True,
-            fn_kwargs={"max_target_length": self.max_target_length},
+            fn_kwargs={"max_target_length": self.max_target_length, "feature_extractor": feature_extractor,
+                       "tokenizer": tokenizer},
             remove_columns=dataset['train'].column_names
         )
 
